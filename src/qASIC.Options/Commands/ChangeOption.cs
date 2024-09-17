c1a3082b8fc0fb3b3e2618209a7b681ea1a7a601
Dock Frankenstein <49789311+DockFrankenstein@users.noreply.github.com>
2024-09-17 11:05:40 +0200

    Removed accidental references in changeoption command

--- src/qASIC.Options/Commands/ChangeOption.cs
+++ src/qASIC.Options/Commands/ChangeOption.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.VisualBasic;
-using qASIC.CommandPrompts;
+﻿using qASIC.CommandPrompts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 
 namespace qASIC.Options.Commands
